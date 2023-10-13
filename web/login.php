a6f23ee62d117fdf2fe615126b20cda8f8a0e74c
eliaskimjong <122789078+eliaskimjong@users.noreply.github.com>
2023-10-13 03:21:27 +0200

    Add files via upload

---
+++ web/login.php
@@ -0,0 +1,23 @@
+<?php
+session_start();
+
+if (isset($_POST['newUserButton'])) {
+    
+}
+
+if (isset($_POST['login'])) {
+    $username = $_POST['username'];
+    $password = $_POST['password'];
+    
+    // You can customize the expected username and password here.
+    $expected_username = '123';
+    $expected_password = '123';
+    
+    if ($username === $expected_username && $password === $expected_password) {
+        $_SESSION['logged_in'] = true;
+        echo "Right password";
+    } else {
+        echo "Wrong username or password. Try again.";
+    }
+}
+?>
